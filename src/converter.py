facf87b1acd90227aa4b9e6bc7b9e602f1159b02
Arkadiy <goryunov.arkadiy@gmail.com>
2023-09-18 11:28:29 +0700

    Add converter

--- src/converter.py
+++ src/converter.py
@@ -1,7 +1,14 @@
 class Converter:
-    def convert(self):
-        pass
+    def convert(self, nominal1: int, value_in_RUB1: float, nominal2: int, value_in_RUB2: float, value: float = 1):
+        currency_in_RUB1 = self.convert_currency_to_rub(nominal1, value_in_RUB1)
+        currency_in_RUB2 = self.convert_currency_to_rub(nominal2, value_in_RUB2)
+
+        multiplier = currency_in_RUB2 / currency_in_RUB1
+        return multiplier * value
+
 
     @staticmethod
-    def currency_to_rub(self):
-        pass
+    def convert_currency_to_rub(nominal: int, value: float):
+        if nominal == 1:
+            return value
+        return value / nominal
--- src/main.py
+++ src/main.py
@@ -1,24 +1,35 @@
-import codecs
 import json
-
-import requests
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from src.config import API_KEY
+from src.converter import Converter
+
 
 app = Flask(__name__)
-
+converter = Converter()
 
 @app.route("/convert")
 def convert():
     # data = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
     # with open('data.json', 'w') as json_file:
     #     json.dump(data, json_file)
+    currency_from = request.args["from"]
+    currency_to = request.args["to"]
+    value = float(request.args["value"])
 
     with open('data.json', 'r') as json_file:
         data = json_file.read()
 
-    data_as_dict = json.loads(data)
-    return data_as_dict
+    data_as_dict = json.loads(data)["Valute"]
+    currency_from_value = data_as_dict[currency_from]["Value"]
+    currency_from_nominal = data_as_dict[currency_from]["Nominal"]
+    currency_to_value = data_as_dict[currency_to]["Value"]
+    currency_to_nominal = data_as_dict[currency_to]["Nominal"]
+
+    return {"Result": converter.convert(currency_from_value,
+                                        currency_from_nominal,
+                                        currency_to_value,
+                                        currency_to_nominal,
+                                        value)}
 
 
 @app.route("/")
@@ -38,7 +49,7 @@ def main():
             {
                 currency: {
                     "Name": description["Name"],
-                    "To RUB": description["Value"]
+                    "To RUB": converter.convert_currency_to_rub(description["Nominal"], description["Value"])
                 }
             }
             for currency, description in zip(data_as_dict.keys(), data_as_dict.values())
